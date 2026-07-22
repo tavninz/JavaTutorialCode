84d576040675f02012a2390ba110a054e651c049
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-22 12:54:25 +0700

    Done Day-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        // println()
         System.out.println("Hello World");
+
+
+        // print()
+
+        System.out.print("Hello World");
+        System.out.print("Java Programming");
+
+        // printf()
+
+        System.out.printf("%.2f", 3.14159); // Outputs: 3.14
+
+        System.out.println("\n");
+
+        System.out.printf("%,d", 1000000); // Outputs: 1,000,000
+        System.out.println("\n");
+
+        // Right-aligned (default): uses 10 spaces total for the name
+        System.out.printf("%10s: $%d\n", "Shoes", 50);
+
+        System.out.println("\n");
+        // Left-aligned: using the minus (-) flag
+        System.out.printf("%-10s: $%d\n", "Shirt", 20);
+
+
     }
 }
---
+++ src/main/resources/Docs.md
@@ -0,0 +1,16 @@
+## Hello World
+
+https://www.w3schools.com/java/ref_output_printf.asp
+
+
+%s for string | Prints a text string.
+
+%d	Integer	Prints whole numbers (byte, short, int, long).
+
+%f	Floating-point	Prints decimals (float, double).
+
+%c	Character	Prints a single character.
+
+%b	Boolean	Prints true or false.
+
+%n or \n	New Line	Moves the cursor to the next line.
