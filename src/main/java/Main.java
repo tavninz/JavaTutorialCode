aecc53650ef4cba572090dcb1615a39a6c06c4c2
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-22 14:20:49 +0700

    Done Day 3 Data Type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Data Type
+
+        // Primitive Data Type
+        // Range: -128 to 127
+        byte age = 25;
+
+        // Range: -32,768 to 32,767
+
+        short year = 2026;
+
+        //Range: Around -2 billion to +2 billion.
+        int studentCount = 25;
+
+        // Used for massive numbers. Uses 8 bytes. Note: You must put an L at the end of the number.
+        long view = 100000000L;
+
+        // Standard decimal numbers. Uses 4 bytes. Note: You must put an f at the end.
+        float price = 5.99f;
+
+        // double The default choice for decimals. Precise up to 15 digits. Uses 8 bytes.
+        double pi = 3.1415926535;;
+
+        //char: Holds a single text character or Unicode value. Uses 2 bytes. Uses single quotes.
+        char grade = 'A';
+
+        // boolean: Holds only two possible values: true or false. Uses 1 bit of logical data.
+        boolean isPassed = true;
+
+        // Non-Primitive Data Type
+
+        //String: A sequence of characters. Uses double quotes.
+        String channelName = "iFDevs";
+
+        //Arrays: Collections of multiple values of the same type.
+        int[] scores = {90, 85, 88};
+
     }
 }
