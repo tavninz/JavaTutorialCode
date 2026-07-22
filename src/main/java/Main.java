d4e2b21bd26597f7f940c105954f33ec844e8b74
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-22 15:14:23 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // 1 Declare Variable
+
+        int age;
+        String name;
+        double price;
+
+        // 2 Declare with initialize value
+
+        int num = 2;
+        String studentName = "Jonh Smith";
+
+
+        // 3 Name Rule
+
+        // 4 Type Variable(Instance Variable, static variable, local variable
+
+        // 5 constant variable
+
     }
 }
