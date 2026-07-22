876c847c343b98567dbb9e73fb7f713a5115ea5c
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-22 19:40:31 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -56,36 +56,5 @@ public class Main {
             default:
                 System.out.println("Invalid day");
         }
-
-
-        // 6- for Loop
-        // Prints: 1 2 3 4 5
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(i + " ");
-        }
-
-        // 7- foreach Loop
-
-        String[] programmingLanguages = {"Java", "Python", "JavaScript"};
-        // Read as: "For each String lang in programmingLanguages"
-        for (String lang : programmingLanguages) {
-            System.out.println("Language: " + lang);
-        }
-
-        // 8- While Loop
-        int count = 1;
-
-        while (count <= 3) {
-            System.out.println("Count is: " + count);
-            count++; // Crucial step to eventually stop the loop
-        }
-
-        // 9- Do While Loop
-
-        int number = 10;
-
-        do {
-            System.out.println("This will print exactly once, even though 10 is not less than 5.");
-        } while (number < 5);
     }
 }
