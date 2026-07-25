4fe264bad2999be723f0ba532b0f2645fc293052
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-25 20:52:20 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+class Fruit {
+    // Property or Field
+    String name;
+    String color;
+    int amount;
+
+    public void display(){
+        System.out.println("Display Method in Class.");
+    }
+    public void showInfo(){
+        System.out.printf("Name :%s, Color: %s, Amount: %d",name,color,amount);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //1- OOP Class and Object
+        Fruit fruit1 = new Fruit();
+
+        // 2- Property/Field
+        fruit1.name = "Apple";
+        fruit1.color = "Red";
+        fruit1.amount = 30;
+
+        System.out.println(fruit1.name);
+        System.out.println(fruit1.color);
+        System.out.println(fruit1.amount);
+
+
+        // 3- Method
+        fruit1.display(); // Call method in class
+        fruit1.showInfo();
     }
 }
