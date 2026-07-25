07addfb3892189d32d88a11a38bb20ae21061d23
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-25 22:41:01 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,47 @@
+
+// Create New Class
+
+class Vehicle {
+    // Create Class Property/Field
+    String name;
+    String color;
+    double price;
+    int amount;
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Create Object of Class using keyword new
+
+        Vehicle vehicle = new Vehicle(); // Create new object from Vehicle class
+
+        // Access to field or property of Class
+        // Give property value
+        vehicle.name = "BMW";
+        vehicle.color = "Black";
+        vehicle.price = 111000.0;
+        vehicle.amount = 1;
+
+        // Display
+
+        System.out.println(vehicle.name);
+        System.out.println(vehicle.color);
+        System.out.println(vehicle.price);
+        System.out.println(vehicle.amount);
+
+
+        // Create Other Object
+
+        Vehicle luxaryCar = new Vehicle();
+        luxaryCar.name = "Lexus";
+        luxaryCar.color = "Silever";
+        luxaryCar.price = 250000;
+        luxaryCar.amount = 5;
+
+        System.out.println(luxaryCar.name);
+        System.out.println(luxaryCar.color);
+        System.out.println(luxaryCar.price);
+        System.out.println(luxaryCar.amount);
+
     }
 }
