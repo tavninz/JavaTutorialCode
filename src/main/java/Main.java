5ab4247521f1a7422563854dfd5c06399923276c
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-26 12:41:07 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+class BankAccount{
+    int accountNumber;
+    String accountName;
+    int firstbalance;
+
+    private int accountBalance;
+
+    //Constructor
+    BankAccount(){
+        accountNumber = 872034343;
+        accountName = "Heatha Rom";
+        firstbalance = 0;
+    }
+
+    // Constructor Parameter
+    BankAccount(int accId,String name, int bal){
+        this.accountNumber = accId;
+        this.accountName = name;
+        this.firstbalance = bal;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        BankAccount b1 = new BankAccount();
+        BankAccount b2 = new BankAccount(6283,"Sroch",5000);
     }
 }
