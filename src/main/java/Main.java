275170893267027d73ffe29c3c20d05eab077cc6
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-26 21:06:34 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        smallResturaunt s1 = new smallResturaunt();
+        s1.purchesOrder(300);
+    }
+}
+
+class Resturaunt {
+    public void purchesOrder(double amount){
+        System.out.println("processing a purches order $" + amount);
+    }
+}
+
+class smallResturaunt extends Resturaunt{
+    @Override
+    public void purchesOrder(double amount) {
+        double fee = amount * 0.02;
+        System.out.println("processing a purches order include fee $" + fee);
     }
 }
