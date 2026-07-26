86e8d7756900b614525d911a9076d7eb56a9d97e
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-26 16:51:55 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,51 @@
+// Inheritance
+class Animal{
+    String name;
+    public  void eat(){
+        System.out.println(name +" is Eating.");
+    }
+}
+class Car{
+    String name;
+    String color;
+}
+
+class Dog extends Animal{
+    public void bark(){
+        System.out.println(name + "is barking.");
+    }
+}
+
+class Cat extends Animal{
+    Cat(String name){
+        super();
+        this.name = name;
+    }
+}
+
+// Multiple Implimentation
+interface Character {
+    void attack();
+}
+interface Weapon {
+    void use();
+}
+class Worrior implements Character,Weapon{
+
+    @Override
+    public void attack() {
+        
+    }
+
+    @Override
+    public void use() {
+
+    }
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
     }
 }
