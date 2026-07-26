ae601dd0cf02cf1ec57252e437c563dc0612e17c
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-26 20:45:56 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Overloading Method : Allow creates multiple method with the same name but differ
+        // parameter
+        A a1 = new A();
+        a1.add();
+        a1.add(2,4);
+        a1.add(2,4,5);
+    }
+}
+
+
+class A {
+    public int a;
+    public int b;
+
+    // Method Same name but difference Parameter
+    public int add(){
+        return a + b;
+    }
+    public int add(int a,int b){
+        return a * b;
+    }
+    public  int add(int a,int b, int c){
+        return a + b * c;
     }
 }
