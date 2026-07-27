cd923ff5584a8ca75f69933778c9fd941ea09e24
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-27 14:02:08 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Child c1 = new Child();
+        c1.TestingChild();
+    }
+}
+
+
+class Parents {
+    // Access Modifiers (private, public, protected)
+    public String publicVar = "Every One Can See.";
+    protected String protectedVar = "See only Same package and same class or child class.";
+    private String privateedVar = "See only current class or class itself.";
+
+    // accessing by using Getter Setter Method on private member
+
+    public String getPrivateedVar() {
+        return privateedVar;
+    }
+
+    public void setPrivateedVar(String privateedVar) {
+        this.privateedVar = privateedVar;
+    }
+}
+
+class Child extends Parents {
+
+    Child(){
+        setPrivateedVar("Value Set from class child.");
+    }
+    public void TestingChild(){
+        System.out.println("1. " + publicVar);
+        System.out.println("2. " + protectedVar);
+        // System.out.println(privatedVar); // Error can not access private member
+
+
+        System.out.println("3. " + getPrivateedVar());
     }
 }
