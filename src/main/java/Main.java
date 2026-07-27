c6217373c9c1fc4564ce091a5ab9301f9aa79145
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-27 15:25:43 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,64 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SmartPOS machine1 = new SmartPOS();
+        machine1.CreditCard();
+
+        SmartPOS machine2 = new SmartPOS();
+        machine2.QRPayway();
+
+        PaymentMethod method1 = new PaymentMethod();
+        method1.ABAPayWay();
+
+        PaymentMethod method2 = new PaymentMethod();
+        method2.AClidaPayWay();
+    }
+}
+
+interface CreditCardPayment{
+    void CreditCard();
+}
+
+interface QRPayment {
+    void QRPayway();
+}
+
+class SmartPOS implements CreditCardPayment, QRPayment {
+    @Override
+    public void CreditCard() {
+        System.out.println("Payment from Credit Card is processing.");
+    }
+
+    @Override
+    public void QRPayway() {
+        System.out.println("KHQR code now is processing.");
+    }
+}
+
+interface ABABankPayment {
+    void ABAPayWay();
+}
+
+interface AClidaBankPayment {
+    void AClidaPayWay();
+}
+
+interface MayBankPayment {
+    void MaybankPayWay();
+}
+
+class PaymentMethod implements ABABankPayment,AClidaBankPayment,MayBankPayment{
+    @Override
+    public void ABAPayWay() {
+        System.out.println("ABA Payment is processing.");
+    }
+
+    @Override
+    public void AClidaPayWay() {
+        System.out.println("AClida Payment is processing.");
+    }
+
+    @Override
+    public void MaybankPayWay() {
+        System.out.println("Maybank Payment is processing.");
     }
 }
