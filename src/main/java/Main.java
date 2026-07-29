610fe269497b3f62bd40223a8c670d55b3e2fb59
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-29 14:24:18 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // 1- Create Map
+        Map<String, Integer> studentScores = new HashMap<>();
+
+        // 2. Add using method put(key, value)
+        studentScores.put("Dara", 85);
+        studentScores.put("Sokha", 92);
+        studentScores.put("Bona", 78);
+
+        // 3- Add duplicate data
+        studentScores.put("Dara", 95);
+
+        // 4- Retrieve data using method get()
+        studentScores.get("Sokha");
+
+        // 5- Checking key or value
+        if(studentScores.containsKey("Bona")){
+            System.out.println("User Nona already exist.");
+        }
+
+        // 6- Remove Key
+        studentScores.remove("Nona");
+
+        // 7-Show Size of studentScores
+        System.out.println(studentScores.size());
+
+        // 7- Loop HashMap
+        studentScores.forEach((name,score)->{
+            System.out.println("Name : "+ name + " Score: " + score);
+        });
     }
 }
