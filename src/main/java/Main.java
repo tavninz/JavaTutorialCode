b8e528b60d8761007320c952f9b8c205651dbbfb
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-29 11:17:55 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Camery");
+        cars.add("Hiyanday");
+        cars.add("Toyota");
+        System.out.println(cars);
+
+        // 3. Methods ពិសេសរបស់ LinkedList (មានប្រយោជន៍សម្រាប់ធ្វើ Queue ឬ Stack)
+        cars.addFirst("Cameron");
+        cars.addLast("VolVo");
+
+        // 4. Get First Item and Last Item
+        cars.getFirst();
+        cars.getLast();
+
+        // 5. Remove First and Last Item
+        cars.removeFirst();
+        cars.removeLast();
     }
 }
