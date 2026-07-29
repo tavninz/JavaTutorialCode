aae5c7ec9ba683920c96a228b1ff4285646a6012
tavninz <74897259+tavninz@users.noreply.github.com>
2026-07-29 14:12:35 +0700

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Create Set & HashSet
+        Set<String> userIds = new HashSet<>();
+
+        // 2. Add items
+        userIds.add("USER_01");
+        userIds.add("USER_02");
+        userIds.add("USER_03");
+
+        // 3- Duplicate key userIds.add("USER_01");// No Dulicate key in Set
+        System.out.println(userIds);
+
+        // 4- Check Key
+        if(userIds.contains("USER_02")){
+            System.out.println("USER_02 ready in Database.");
+        }
+
+        // 5- Remove
+        userIds.remove("USER_02");
+
+        // 6- Check Size of
+        System.out.println(userIds.size());
+
+
+        // Create HashSet
+
     }
 }
